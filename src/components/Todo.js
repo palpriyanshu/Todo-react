0ce3c5d666bbff3087a0b14c1f3321b8814fb33e
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-08-29 19:53:56 +0530

    move App components to components directory

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Todo = (props) => {
+  const className = props.hasDone ? 'todoBox done' : 'todoBox todo';
+  return (
+    <div className={className} onClick={() => props.onClick(props.id)}>
+      {props.item}
+    </div>
+  );
+};
+
+export default Todo;
