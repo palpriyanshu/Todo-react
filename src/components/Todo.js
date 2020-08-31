26f08bbec7793ba2469689c572895c88146ada3a
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-08-31 12:47:02 +0530

    added new state in TodoList

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const Todo = (props) => {
-  const className = props.hasDone ? 'todoBox done' : 'todoBox todo';
+  let className = 'todo';
+  let status = { isInProgress: true, isDone: false };
+
+  if (props.isInProgress) {
+    className = 'inProgress';
+    status = { isInProgress: false, isDone: true };
+  }
+
+  if (props.isDone) {
+    className = 'done';
+    status = { isInProgress: false, isDone: false };
+  }
+
   return (
-    <div className={className} onClick={() => props.onClick(props.id)}>
-      {props.item}
+    <div
+      className={`todoBox ${className}`}
+      onClick={() => props.onClick(props.id, status)}
+    >
+      {props.task}
     </div>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -10,11 +10,12 @@ class TodoList extends React.Component {
     this.addTask = this.addTask.bind(this);
   }
 
-  updateTaskStatus(taskId) {
+  updateTaskStatus(taskId, { isInProgress, isDone }) {
     this.setState((state) => {
       let todoList = state.todoList.slice();
       const task = Object.assign({}, todoList[taskId]);
-      task.hasDone = !task.hasDone;
+      task.isInProgress = isInProgress;
+      task.isDone = isDone;
       todoList[taskId] = task;
       return { todoList };
     });
@@ -23,21 +24,24 @@ class TodoList extends React.Component {
   addTask(task) {
     this.setState((state) => {
       let todoList = state.todoList.slice();
-      todoList.push({ task, hasDone: false });
+      todoList.push({ task, isInProgress: false, isDone: false });
       return { todoList };
     });
   }
 
   render() {
-    const items = this.state.todoList.map(({ task, hasDone }, id) => (
-      <Todo
-        item={task}
-        id={id}
-        onClick={this.updateTaskStatus}
-        hasDone={hasDone}
-        key={id}
-      />
-    ));
+    const items = this.state.todoList.map(
+      ({ task, isInProgress, isDone }, id) => (
+        <Todo
+          task={task}
+          id={id}
+          onClick={this.updateTaskStatus}
+          isDone={isDone}
+          isInProgress={isInProgress}
+          key={id}
+        />
+      )
+    );
 
     return (
       <div>
